58e62f832bdbee19a03c84f646368fa2c217ff61
Soojung Hong <soojung.hong@xsenda.ch>
2018-03-20 17:13:38 +0100

    append to partitioned Hive table

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
 import org.apache.spark.sql.functions._
 
 import scala.math._
@@ -17,6 +17,9 @@ object SimpleApp {
       val conf = new SparkConf().setAppName("Simple Application")
       val sc = new SparkContext(conf)
       val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+      sqlContext.setConf("hive.exec.dynamic.partition", "true")
+      sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
+
       import sqlContext.implicits._
 
       //-------------------------------------------------
@@ -30,58 +33,34 @@ object SimpleApp {
       println("=============================== DEBUG select columns")
       selectedCols.show()
 
-      val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", lit("100"))
-      println("=============================== DEBUG select columns in cooked_pea_account")
-      new_peaAccountRawDF.show()
+      //val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", lit("100"))
+      //println("=============================== DEBUG show new_peaAccountRawDF")
+      //new_peaAccountRawDF.show()
 
-      // data.write.mode("append").insertInto("my_table")
-      new_peaAccountRawDF.select(new_peaAccountRawDF.col("party_id"), new_peaAccountRawDF.col("building_port_id"), new_peaAccountRawDF.col("cpe_equipment_id"), new_peaAccountRawDF.col("cpe_port_id"), new_peaAccountRawDF.col("cpe_mac"), new_peaAccountRawDF.col("trigger"), new_peaAccountRawDF.col("ts_created"), new_peaAccountRawDF.col("ts_received"), new_peaAccountRawDF.col("meta_past_id"), new_peaAccountRawDF.col("score"), new_peaAccountRawDF.col("root_id"), new_peaAccountRawDF.col("date"), new_peaAccountRawDF.col("hour")).write.mode("append").insertInto("cooked_pea_account")
+      //following could not parsed & insert statement for HIVE is too slow
+      //sqlContext.sql("insert into cooked_pea_account1 partition (date='2018-01-01',hour=02) values (228918879,92017266,3445801,13671760,'481C4CC6B7A','CUSTOMER','2018-01-01 00:57:25','2018-03-15 13:33:06',2,100.0,112354305)")
 
-      println("=============================== DEBUG select columns in cooked_pea_account")
+      //val hiveDF = new_peaAccountRawDF.select(new_peaAccountRawDF.col("party_id"), new_peaAccountRawDF.col("building_port_id"), new_peaAccountRawDF.col("cpe_equipment_id"), new_peaAccountRawDF.col("cpe_port_id"), new_peaAccountRawDF.col("cpe_mac"), new_peaAccountRawDF.col("trigger"), new_peaAccountRawDF.col("ts_created"), new_peaAccountRawDF.col("ts_received"), new_peaAccountRawDF.col("meta_past_id"), new_peaAccountRawDF.col("score"), new_peaAccountRawDF.col("root_id"), new_peaAccountRawDF.col("date"), new_peaAccountRawDF.col("hour"))
+      val hiveDF = calcScoreValue(peaAccountRawDF)
+      hiveDF.write.mode(SaveMode.Append).partitionBy("date", "hour").saveAsTable("cooked_pea_account")
+
+      println("=============================== DEBUG SELECT * FROM cooked_pea_account ===================")
       val appendCookedPea = sqlContext.sql("SELECT * FROM cooked_pea_account")
       appendCookedPea.show()
-      /*
-      //---------------
-      // Spark Context
-      //---------------
-      val conf = new SparkConf().setAppName("Simple Application")
-      val sc = new SparkContext(conf)
-      val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
-      import sqlContext.implicits._
-
-      //--------------------------------------
-      // Read raw peas in Hive table 'post39'
-      //--------------------------------------
-      val readStart = System.nanoTime()
-      // The results of SQL queries are themselves DataFrames and support all normal functions.
-      val rawPeasDF = sqlContext.sql("SELECT * FROM post39")
-      val readEnd = System.nanoTime()
-      println("============= Elapsed Time : " + "Reading " + rawPeasDF.count() + " took " + (readEnd - readStart)/1000000000 + " in second")
-      rawPeasDF.show()
-
-      //--------------------------------------------------
-      // Apply Score function to column 'x1' in each pea
-      //--------------------------------------------------
-      import org.apache.spark.sql.functions.udf
-      val myScoreFunc = udf(scoreFunction _)
-      val cookedPeasDF = rawPeasDF.withColumn("y", myScoreFunc(rawPeasDF.col("x1")))
-
-      //-------------------------------------------------------------
-      // Write cooked peas using score function in Hive table 'peas'
-      //-------------------------------------------------------------
-      val writeStart = System.nanoTime()
-      cookedPeasDF.select(cookedPeasDF.col("x1"), cookedPeasDF.col("y")).write.mode("overwrite").saveAsTable("peas");
-      val writeEnd = System.nanoTime() //============= Elapsed Time : Writing 20000000 took 13 in second
-      println("============= Elapsed Time : " + "Writing " + cookedPeasDF.count() + " took " + (writeEnd - writeStart)/1000000000 + " in second")
-
-      val df = sqlContext.sql("SELECT * FROM peas")
-      df.show()
-      */
 
     } else {
         createAndTestLocalDataFrame()
     }
 
+  }
+
+  def calcScoreValue(peaAccountRawDF : DataFrame): DataFrame = {
+    import org.apache.spark.sql.functions.udf
+    val stScoreFunc = udf(speedTestScoreFunction _)
+    val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", stScoreFunc(peaAccountRawDF.col("technical_value") * peaAccountRawDF.col("cust_relevance")))
+    val simplifiedCookedPeasDF = new_peaAccountRawDF.select(new_peaAccountRawDF.col("party_id"), new_peaAccountRawDF.col("building_port_id"), new_peaAccountRawDF.col("cpe_equipment_id"), new_peaAccountRawDF.col("cpe_port_id"), new_peaAccountRawDF.col("cpe_mac"), new_peaAccountRawDF.col("trigger"), new_peaAccountRawDF.col("ts_created"), new_peaAccountRawDF.col("ts_received"), new_peaAccountRawDF.col("meta_past_id"), new_peaAccountRawDF.col("score"), new_peaAccountRawDF.col("root_id"), new_peaAccountRawDF.col("date"), new_peaAccountRawDF.col("hour"))
+
+    simplifiedCookedPeasDF
   }
 
   def readTestHive(): Unit = {
@@ -107,7 +86,7 @@ object SimpleApp {
     // Apply Score function to column 'x1' in each pea
     //--------------------------------------------------
     import org.apache.spark.sql.functions.udf
-    val myScoreFunc = udf(scoreFunction _)
+    val myScoreFunc = udf(speedTestScoreFunction _)
     val cookedPeasDF = rawPeasDF.withColumn("y", myScoreFunc(rawPeasDF.col("x1")))
 
     //-------------------------------------------------------------
@@ -123,7 +102,7 @@ object SimpleApp {
 
   }
 
-  def scoreFunction (x : Int) : Double = {
+  def speedTestScoreFunction (x : Int) : Double = {
     val zeroThresh = 80
     val slopePos = 0.1
     val slopeNeg = 0.6
@@ -195,7 +174,7 @@ object SimpleApp {
     //apply user defined function
     import org.apache.spark.sql.functions.udf
     val myFunc = udf(simpleFunc _)
-    val myScoreFunc = udf(scoreFunction _)
+    val myScoreFunc = udf(simpleFunc _)
 
     //add column y after apply function
     println("... update the 'newCol' with value of x1 * x2")
