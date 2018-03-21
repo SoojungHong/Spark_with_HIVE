9e824adba51fd9f79957fc894018b09575a4114f
Soojung Hong <soojung.hong@xsenda.ch>
2018-03-21 17:03:12 +0100

    append to HIVE table

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -5,8 +5,9 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
 import org.apache.spark.sql.functions._
 
+import com.databricks.spark.csv._
+
 import scala.math._
-import BigInt._
 
 object SimpleApp {
 
@@ -22,6 +23,10 @@ object SimpleApp {
       val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
       sqlContext.setConf("hive.exec.dynamic.partition", "true")
       sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
+      sqlContext.setConf("hive.exec.max.dynamic.partitions.pernode", "1000")
+      sqlContext.setConf("hive.enforce.bucketing", "true")
+      sqlContext.setConf("hive.exec.compress.output", "true")
+      sqlContext.setConf("spark.sql.hive.convertMetastoreParquet", "false")
 
       import sqlContext.implicits._
 
@@ -29,25 +34,26 @@ object SimpleApp {
       // Read raw peas in Hive table 'pea_account_raw'
       //-------------------------------------------------
       val peaAccountRawDF= sqlContext.sql("SELECT * FROM pea_account_raw")
-      println("=============================== DEBUG (SELECT * FROM pea_account_raw")
+      println("....... [DEBUG] (SELECT * FROM pea_account_raw")
       peaAccountRawDF.show()
 
       val selectedCols = peaAccountRawDF.select(peaAccountRawDF.col("meta_past_id"), peaAccountRawDF.col("technical_value"), peaAccountRawDF.col("cust_relevance") )
-      println("=============================== DEBUG select columns")
+      println(".......[DEBUG] select columns")
       selectedCols.show()
-
-      //val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", lit("100"))
-      //println("=============================== DEBUG show new_peaAccountRawDF")
-      //new_peaAccountRawDF.show()
 
       //following could not parsed & insert statement for HIVE is too slow
       //sqlContext.sql("insert into cooked_pea_account1 partition (date='2018-01-01',hour=02) values (228918879,92017266,3445801,13671760,'481C4CC6B7A','CUSTOMER','2018-01-01 00:57:25','2018-03-15 13:33:06',2,100.0,112354305)")
 
       //val hiveDF = new_peaAccountRawDF.select(new_peaAccountRawDF.col("party_id"), new_peaAccountRawDF.col("building_port_id"), new_peaAccountRawDF.col("cpe_equipment_id"), new_peaAccountRawDF.col("cpe_port_id"), new_peaAccountRawDF.col("cpe_mac"), new_peaAccountRawDF.col("trigger"), new_peaAccountRawDF.col("ts_created"), new_peaAccountRawDF.col("ts_received"), new_peaAccountRawDF.col("meta_past_id"), new_peaAccountRawDF.col("score"), new_peaAccountRawDF.col("root_id"), new_peaAccountRawDF.col("date"), new_peaAccountRawDF.col("hour"))
       val hiveDF = calcScoreValue(peaAccountRawDF)
-      hiveDF.write.mode(SaveMode.Append).partitionBy("date", "hour").saveAsTable("cooked_pea_account")
+      //could make partitions and save files in parquet
+      //OK - BUT spark table not hive table//hiveDF.write.mode(SaveMode.Overwrite).option("compression", "gzip").partitionBy("date", "hour").saveAsTable("cooked_pea_account") //OK
+      hiveDF.write.mode(SaveMode.Overwrite).option("compression", "gzip").partitionBy("date", "hour").insertInto("cooked_pea_account") //OK
 
-      println("=============================== DEBUG SELECT * FROM cooked_pea_account ===================")
+      //write as csv
+      //hiveDF.write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").partitionBy("date", "hour").insertInto("cooked_pea_account") //OK
+
+      println("....... [DEBUG] SELECT * FROM cooked_pea_account")
       val appendCookedPea = sqlContext.sql("SELECT * FROM cooked_pea_account")
       appendCookedPea.show()
 
@@ -60,12 +66,84 @@ object SimpleApp {
   def calcScoreValue(peaAccountRawDF : DataFrame): DataFrame = {
     import org.apache.spark.sql.functions.udf
     val scoreFunc = udf(selectAndCalcScoreFunction _)
-    //val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", stScoreFunc(peaAccountRawDF.col("technical_value") * peaAccountRawDF.col("cust_relevance")))
-    val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", scoreFunc(peaAccountRawDF.col("technical_value"), peaAccountRawDF.col("cust_relevance"), peaAccountRawDF.col("meta_past_id") ))
+    val new_peaAccountRawDF = peaAccountRawDF.withColumn("score", scoreFunc(peaAccountRawDF.col("technical_value"), peaAccountRawDF.col("cust_relevance"), peaAccountRawDF.col("meta_past_id")))
     val simplifiedCookedPeasDF = new_peaAccountRawDF.select(new_peaAccountRawDF.col("party_id"), new_peaAccountRawDF.col("building_port_id"), new_peaAccountRawDF.col("cpe_equipment_id"), new_peaAccountRawDF.col("cpe_port_id"), new_peaAccountRawDF.col("cpe_mac"), new_peaAccountRawDF.col("trigger"), new_peaAccountRawDF.col("ts_created"), new_peaAccountRawDF.col("ts_received"), new_peaAccountRawDF.col("meta_past_id"), new_peaAccountRawDF.col("score"), new_peaAccountRawDF.col("root_id"), new_peaAccountRawDF.col("date"), new_peaAccountRawDF.col("hour"))
 
     simplifiedCookedPeasDF
   }
+
+
+  def selectAndCalcScoreFunction(x1 : Double, x2 : Double, x3 : Int) : Double = {
+    var score = 0.0
+    if(x3 == 2) { //if speedtest is 2
+      score = speedTestScoreFunction(x1 * x2)
+    } else if(x3 == 1) {
+      score = HFCScoreFunction(x1 * x2)
+    } //else call default function or pass default value
+
+    score
+  }
+
+
+  def speedTestScoreFunction (x : Double) : Double = {
+    val zeroThresh = 80
+    val slopePos = 0.1
+    val slopeNeg = 0.6
+    val maxPos = 5
+    val maxNeg = -2
+    var y:Double = 0
+
+    val factorPos = maxPos/scala.math.atan(slopePos*(100 - zeroThresh))
+    val factorNeg = maxNeg/scala.math.atan(-slopeNeg * (zeroThresh))
+
+    if (x > zeroThresh) {
+      y = factorPos * scala.math.atan(slopePos*(x-zeroThresh))
+    }
+    else {
+      y = factorNeg * scala.math.atan(-slopeNeg * (zeroThresh-x))
+    }
+
+    return(y)
+  }
+
+  def HFCScoreFunction (x : Double) : Double = {
+    var score = 0.0
+
+    val slope = 1.2
+    val maxPos = 0
+    val maxMin = -2
+
+    score = (maxPos - maxMin) * math.exp(-slope * x) - (maxPos - maxMin) + maxPos
+    return(score)
+  }
+
+
+/*
+  def createAndTestLocalDataFrame(): Unit = {
+    System.setProperty("hadoop.home.dir", "c:\\winutil\\")
+
+    val conf = new SparkConf().setAppName("Local Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
+
+    //create sample dataframe
+    val df = Seq((12, 23), (888, 44), (2, 6), (19, 233), (98, 100)).toDF("x1", "x2")
+    df.show()
+
+    //apply user defined function
+    import org.apache.spark.sql.functions.udf
+    val myFunc = udf(simpleFunc _)
+
+    import org.apache.spark.sql.functions.udf
+    val myScoreFunc = udf(scoreFunction _)
+
+    //add column y after apply function
+    val newDF = df.withColumn("y", myScoreFunc(df.col("x1")))
+    newDF.show()
+  }
+*/
+
 
   def readTestHive(): Unit = {
     //---------------
@@ -106,65 +184,6 @@ object SimpleApp {
 
   }
 
-  def speedTestScoreFunction (x : Double) : Double = {
-    val zeroThresh = 80
-    val slopePos = 0.1
-    val slopeNeg = 0.6
-    val maxPos = 5
-    val maxNeg = -2
-    var y:Double = 0
-
-    val factorPos = maxPos/scala.math.atan(slopePos*(100 - zeroThresh))
-    val factorNeg = maxNeg/scala.math.atan(-slopeNeg * (zeroThresh))
-
-    if (x > zeroThresh) {
-      y = factorPos * scala.math.atan(slopePos*(x-zeroThresh))
-    }
-    else {
-      y = factorNeg * scala.math.atan(-slopeNeg * (zeroThresh-x))
-    }
-
-    return(y)
-  }
-
-  def HFCScoreFunction (x : Double) : Double = {
-    val ret = 0.1
-    ret
-  }
-
-  def selectAndCalcScoreFunction(x1 : Double, x2 : Double, x3 : Int) = {
-    if(x3 == 2) { //if speedtest is 2
-      speedTestScoreFunction(x1 * x2)
-    } else if(x3 == 1) {
-      HFCScoreFunction(x1 * x2)
-    } //else call other function
-  }
-
-/*
-  def createAndTestLocalDataFrame(): Unit = {
-    System.setProperty("hadoop.home.dir", "c:\\winutil\\")
-
-    val conf = new SparkConf().setAppName("Local Application").setMaster("local")
-    val sc = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.implicits._
-
-    //create sample dataframe
-    val df = Seq((12, 23), (888, 44), (2, 6), (19, 233), (98, 100)).toDF("x1", "x2")
-    df.show()
-
-    //apply user defined function
-    import org.apache.spark.sql.functions.udf
-    val myFunc = udf(simpleFunc _)
-
-    import org.apache.spark.sql.functions.udf
-    val myScoreFunc = udf(scoreFunction _)
-
-    //add column y after apply function
-    val newDF = df.withColumn("y", myScoreFunc(df.col("x1")))
-    newDF.show()
-  }
-*/
 
   def createAndTestLocalDataFrame(): Unit = {
     System.setProperty("hadoop.home.dir", "c:\\winutil\\")
