0ce257ef46fb370416425ad87f42b46387778069
Soojung Hong <soojung.hong@xsenda.ch>
2018-03-29 16:33:42 +0200

    join dataframe

--- src/main/scala/util/PostgresConnector.scala
+++ src/main/scala/util/PostgresConnector.scala
@@ -15,14 +15,37 @@ object PostgresConnector {
     val conf = new SparkConf().setAppName("Postgres Application").setMaster("local")
     val sc = new SparkContext(conf)
 
+    //read column example using select()
     val sqlContext: SQLContext = new SQLContext(sc)
     val df_records = sqlContext.read.format("jdbc").option("url", "jdbc:postgresql://192.168.21.16/rubcom")
       .option("driver", "org.postgresql.Driver")
       .option("dbtable", "dev_inthub_meta_unity_spark.meta_pea_account_subtype")//"public.records")
       .option("user", "rubcom")
-      .option("password", "2009Sent").load().select("scd_is_active")
+      .option("password", "2009Sent").load().select("meta_past_id")
 
     df_records.printSchema()
     df_records.show()
+
+    //read whole table using load()
+    val df = sqlContext.read.format("jdbc").option("url", "jdbc:postgresql://192.168.21.16/rubcom")
+      .option("driver", "org.postgresql.Driver")
+      .option("dbtable", "dev_inthub_meta_unity_spark.meta_pea_account_subtype")
+      .option("user", "rubcom")
+      .option("password", "2009Sent").load()
+
+    df.show()
+
+    //read only 'meta_past_id', 'meta_pat_id', 'meta_past_halflife', 'scd_is_active', 'scd_is_deleted'
+    val selectedDF = df.select(df.col("meta_past_id"), df.col("meta_pat_id"), df.col("meta_past_halflife"), df.col("scd_is_active"), df.col("scd_is_deleted"))
+    selectedDF.show()
+
+    //join two dataframes
+    val df_l = df_records.as("left_table")
+    val df_r = df.as("right_table")
+    val joined_table = df_l.join(df_r, df_l.col("meta_past_id") === df_r.col("meta_past_id"), "inner")
+    joined_table.show()
+
+    //joined_table.select(joined_table.col())
+
   }
 }
